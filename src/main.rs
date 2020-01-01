24af895feca4e9ba3c6352e09f92d8df438388d8
root <root@DESKTOP-FD83HE5.localdomain>
2020-01-01 22:05:15 +0900

    postgress の 非同期処理とtokioの相性が悪い?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate hyper;
+extern crate postgres;
 extern crate tokio;
-use std::env;
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 
@@ -40,13 +40,33 @@ async fn response_examples(
 
 #[tokio::main]
 async fn main() -> Result<(), GenericError> {
-    let port = match env::var("PORT") {
+    let postgres_password = std::env::var("POSTGRES_PASSWORD").unwrap();
+    postgres::Config::new()
+        .user("postgres")
+        .password(postgres_password)
+        .connect(postgres::NoTls);
+
+    // match postgres::Config::new()
+    //     .user("postgres")
+    //     .password(postgres_password)
+    //     .connect(postgres::NoTls)
+    // {
+    //     Ok(mut connection) => match connection.execute("create table user(name text)", &[]) {
+    //         Ok(number) => {
+    //             println!("postgressへの書き込み成功 {}", number);
+    //         }
+    //         Err(e) => {
+    //             println!("postgressへの書き込み失敗 {}", e);
+    //         }
+    //     },
+    //     Err(e) => println!("postgressと接続できなかった{}", e),
+    // }
+
+    let port = match std::env::var("PORT") {
         Ok(value) => value.parse::<u16>().unwrap(),
         Err(_) => 3000,
     };
-
     let address = ([0, 0, 0, 0], port).into();
-
     let service = hyper::service::make_service_fn(move |_| {
         async {
             Ok::<_, GenericError>(hyper::service::service_fn(move |req| {
@@ -55,13 +75,9 @@ async fn main() -> Result<(), GenericError> {
             }))
         }
     });
-
     // Then bind and serve...
     let server = hyper::Server::bind(&address).serve(service);
-
     println!("Listening on http://{}", address);
-
     server.await?;
-
     Ok(())
 }
