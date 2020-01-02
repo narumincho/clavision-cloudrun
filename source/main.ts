c500ad82228edefc48902ecdd86c7e8d2e6f3a23
narumincho <narumincho.starfy@gmail.com>
2020-01-02 20:44:39 +0900

    connect postgres

--- source/main.ts
+++ source/main.ts
@@ -1,6 +1,13 @@
 import * as express from "express";
+import * as pg from "pg";
 
 const app = express();
+
+const client = new pg.Client({
+  user: "postgres",
+  password: process.env.POSTGRES_PASSWORD
+});
+client.connect();
 
 const port = process.env.PORT;
 console.log(port);
@@ -26,6 +33,10 @@ app.get("/", (request, response) => {
         </body>
         
         </html>`);
+});
+app.post("/p", async (request, response) => {
+  const result = await client.query(request.body);
+  response.contentType("application/json").send(JSON.stringify(result));
 });
 app.listen(port ?? 3000, () => {
   console.log("port番号" + port + "で受付中");
